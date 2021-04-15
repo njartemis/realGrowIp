2a5776a5ee373b32b7469de708d3226449a887a2
@Robertas64 <atomicstoreid@gmail.com>
2021-04-15 06:21:20 +0000

    Create ipfinder.py

---
+++ ipfinder.py
@@ -0,0 +1,27 @@
+#   ___ ___     ___ _         _         
+# |_ _| _ \___| __(_)_ _  __| |___ _ _ 
+#  | ||  _/___| _|| | ' \/ _` / -_) '_|
+# |___|_|     |_| |_|_||_\__,_\___|_|  
+# Made by Robertas64
+
+
+
+#Importing the module
+import os
+from time import *
+
+banner = """
+  ___ ___     ___ _         _         
+ |_ _| _ \___| __(_)_ _  __| |___ _ _ 
+  | ||  _/___| _|| | ' \/ _` / -_) '_|
+ |___|_|     |_| |_|_||_\__,_\___|_|  
+                    
+    Find GrowtopiaServer Real IP
+    Author       :    Robertas64
+     Make sure you're connected
+     To GrowtopiaServer hosts
+"""
+
+#Main
+print(banner)
+os.system("ping growtopia1.com")
